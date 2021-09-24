ea2f6118efc5ef1890922c609e4d377b709dc338
Atsocs <atsocs.cc@gmail.com>
2021-09-24 00:22:58 -0300

    merging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './scripts/searchbar.js'
+import Mensagem from './Mensagem.js'
 
 function App() {
 
   return (
     <div className="App">
       <SearchBar></SearchBar>
+      <Mensagem uf="SP" municipio="São José dos Campos" focosCount="3"></Mensagem>
     </div>
   );
 }
