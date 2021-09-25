f5bffba1e606c6d50fde5dcdc208a865ba5141f8
Atsocs <atsocs.cc@gmail.com>
2021-09-25 01:22:01 -0300

    filters without accents

--- server/utils/check_city.js
+++ server/utils/check_city.js
@@ -1,9 +1,10 @@
 // check focos on json list by city
 const jsonData = require('./read_data')
+const retira_acentos = require('./retira_acentos')
 
 const cityFilter = (city) => {
     const focos = jsonData.features.filter((foco) => {
-        return foco.properties.municipio === city
+        return retira_acentos(foco.properties.municipio) === retira_acentos(city)
     })
     return {
         focos: focos,
--- server/utils/check_state.js
+++ server/utils/check_state.js
@@ -1,9 +1,10 @@
 // check focos on json list by state
 const jsonData = require('./read_data')
+const retira_acentos = require('./retira_acentos')
 
 const stateFilter = (state) => {
     const focos = jsonData.features.filter((foco) => {
-        return foco.properties.estado === state
+        return retira_acentos(foco.properties.estado) === retira_acentos(state)
     })
     
     return {
