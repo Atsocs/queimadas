3522f3a68d6450a429d57e74240903f8995424e9
Atsocs <atsocs.cc@gmail.com>
2021-09-25 16:26:47 -0300

    show fireCount from user location

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,8 @@ function App() {
 
   return (
     <div className="App">
-      <SearchBar></SearchBar>
-      <FireBoard
-        // notFound // uncomment this to pass notFound to the component
-        stateCode="AC"
-        cityName="Rio Branco"
-        countryFires="1717"
-        stateFires="43"
-        cityFires="12"
-      ></FireBoard>
+      <SearchBar />
+      <FireBoard />
     </div>
   );
 }
