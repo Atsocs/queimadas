8cf867c3942214ffa28dd0c91d23e186929efd7b
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-09-24 22:21:54 -0300

    City filter finished

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const cityFilter = require('./utils/check_city')
 
 const PORT = process.env.PORT || 3001;
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Fire check Api" });
 });
+
+app.post("/api/city", (req, res) => {
+    const body = req.body
+    if(body){
+        const {focos, num_focos} = cityFilter(body.city)
+        res.json({ num_focos: num_focos, focos: focos })        
+    }
+    else{
+        res.json(400, { message: 'Wrong request' })
+    }
+})
 
 app.listen(PORT, () =>{
     console.log(`Server listening on ${PORT}`);
 })
-
-console.log('Numero focos:', cityFilter.num_focos)
--- server/utils/check_city.js
+++ server/utils/check_city.js
@@ -1,13 +1,14 @@
 // check if city is in json list
 const jsonData = require('./read_data')
 
-const city_teste = "BRASILÉIA"
-
-const focos = jsonData.features.filter((element) => {
-    return element.properties.municipio === city_teste
-})
-
-module.exports = {
-    focos: focos,
-    num_focos: focos.length
+const cityFilter = (city) => {
+    const focos = jsonData.features.filter((foco) => {
+        return foco.properties.municipio === city
+    })
+    return {
+        focos: focos,
+        num_focos: focos.length
+    }
 }
+
+module.exports = cityFilter
