7f070d2613d65329ee8fcb1d2e386fce9530908a
ArthurJose2000 <arthur2000jose@gmail.com>
2021-11-27 17:18:40 -0300

    fixing calls to api - state and city

--- server/utils/check_city.js
+++ server/utils/check_city.js
@@ -2,9 +2,9 @@
 const jsonData = require('./read_data')
 const retira_acentos = require('./retira_acentos')
 
-const cityFilter = (city, state) => {
+const cityFilter = (city) => {
     const focos = jsonData.features.filter((foco) => {
-        return retira_acentos(foco.properties.municipio) === retira_acentos(city) && retira_acentos(foco.properties.estado) === retira_acentos(state)
+        return retira_acentos(foco.properties.municipio) === retira_acentos(city)
     })
     return {
         focos: focos,
