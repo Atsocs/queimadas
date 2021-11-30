0fab78b75710e3bee11d774ff4b65d3f7d5b68f4
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-11-30 00:25:45 -0300

    Fetch Data from INPE

---
+++ data/fetchData/constants.js
@@ -0,0 +1,7 @@
+dataUrl = "https://queimadas.dgi.inpe.br/home/download?id=focos_brasil&time=48h&outputFormat=json&utm_source=landing-page&utm_medium=landing-page&utm_campaign=dados-abertos&utm_content=focos_brasil_48h"
+destinationPath = "./data/focos48h_Brasil.json"
+
+module.exports = { 
+    dataUrl, 
+    destinationPath 
+}
